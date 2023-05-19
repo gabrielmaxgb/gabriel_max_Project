9dcec4aacb42a4727764867caff3b3fc3135e3a2
Gabriel Max <gmaxgomes@gmail.com>
2023-05-18 21:59:59 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import AppRoutes from "./routes/AppRoutes";
+import "./App.css";
+import Container from "./components/layout/container/Container";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <Container>
+      <AppRoutes />
+    </Container>
+  );
 }
 
 export default App;
